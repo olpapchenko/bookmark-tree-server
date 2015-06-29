2031e14a0c393c167b27fbde322b71d23f1ba108
Oleksandr <rocketmannot@gmail.com>
2015-06-29 16:15:08 +0300

    [~] DB connection data changed

--- config/db/bookshelf.js
+++ config/db/bookshelf.js
@@ -2,10 +2,10 @@
 var dbConfig = {
     client: 'pg',
     connection: {
-        host     : '127.0.0.1',
-        user     : 'postgres',
-        password : 'tree',
-        database : '31107548'
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'Ur@n!um824',
+        database: 'tree'
     }
 }
 
