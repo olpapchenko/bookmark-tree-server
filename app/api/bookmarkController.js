95e292652a3827e14dd634108e0c3ff6b035488b
Oleksandr <rocketmannot@gmail.com>
2015-06-30 13:59:06 +0300

    [+] bookmark crud

--- app/api/bookmarkController.js
+++ app/api/bookmarkController.js
@@ -1,5 +1,23 @@
+var bookmark = require("../models/bookmark");
+var user = require("../models/user");
+
+module.all = function (req, resp) {
+    new user({id: req.session.userId}).fetch({
+        withRelated: ['bookmarks']
+    }).then(function (user) {
+        resp.json(user.related('bookmarks'));
+    } );
+}
+
 module.exports.get = function (req, resp) {
-    resp.send("bookmark");
+    user.where({id: req.params.id}).fetch({
+        withRelated: ['users']
+    }).then(function (b) {
+        if(b.related('users').filter(function (c) {return c.id == res.session.userId})){
+            resp.json(b);
+        } else
+        resp.json({});
+    });
 }
 
 module.exports.put = function (req, resp) {
@@ -7,5 +25,16 @@ module.exports.put = function (req, resp) {
 }
 
 module.exports.post = function (req, resp){
-
+    if(req.body.name && req.body.url){
+        new bookmark(req.body).save().then(
+            function (model) {
+                resp.json(model);
+            },
+            function () {
+                resp.status(500).send("Internal server error contact your administrator");
+            }
+        );
+    } else {
+        resp.status(400).send("name and url can not be empty");
+    }
 }
