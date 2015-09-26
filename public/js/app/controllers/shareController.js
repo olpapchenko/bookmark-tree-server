d682e16e7129950de2fd4afbc71455968d844d07
Oleksandr <rocketmannot@gmail.com>
2015-09-26 20:49:00 +0300

    [+] draggable

--- public/js/app/controllers/shareController.js
+++ public/js/app/controllers/shareController.js
@@ -6,7 +6,6 @@ angular.module("app").controller("shareController",["$scope",
     $scope.shareWith = [];
 
     persistanceService.getShareInformation($scope.id).then(function(data){
-        console.log(data);
         $scope.owners = data.owners;
         $scope.observers = data.observers;
     });
@@ -22,14 +21,12 @@ angular.module("app").controller("shareController",["$scope",
         });
     }
 
-    $scope.addToShare = function(friend) {
-        if( !($scope.shareWith.some(function(item){ return item.id == friend.id}))){
-            $scope.shareWith.push(friend);
-        }
+    $scope.removeFromOwnerShip = function (friend) {
+        $scope.owners.splice($scope.owners.indexOf(friend), 1);
     }
 
-    $scope.removeFromShare = function (friend) {
-        $scope.shareWith.splice($scope.shareWith.indexOf(friend), 1);
+    $scope.removeFromObservers = function (friend) {
+        $scope.observers.splice($scope.observers.indexOf(friend), 1);
     }
 
     $scope.$watch("name", function(){
