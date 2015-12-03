6388669e00f00b22bdc8aa209be1a92ba8c1c0cd
Oleksandr <rocketmannot@gmail.com>
2015-12-03 20:47:32 +0200

    [+] ejs support

--- config/assetsPipelineEnvironment.js
+++ config/assetsPipelineEnvironment.js
@@ -1,11 +1,22 @@
 var mincer = require('mincer'),
-    app_config = require("./app_config");
+    appConfig = require("./app_config"),
+    path = require('path');
 
 var mincerEnvironment = new mincer.Environment();
-mincerEnvironment.appendPath(app_config.assetsPath);
+mincerEnvironment.appendPath(appConfig.assetsPath);
 
-if(app_config.mode == "production") {
+if(appConfig.mode == "production") {
     mincerEnvironment.jsCompressor  = 'uglify';
 }
+
+mincerEnvironment.ContextClass.defineAssetPath(function (pathname, options) {
+    var asset = this.environment.findAsset(pathname, options);
+
+     if (!asset) {
+        throw new Error('File ' + pathname + ' not found');
+    }
+
+    return  path.join("/assets", asset.digestPath).replace(/\\/g, "/");
+});
 
 module.exports = mincerEnvironment;
