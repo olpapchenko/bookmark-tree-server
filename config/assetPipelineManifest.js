200970aff9c14d0539fa47c88e67e0896362d530
Oleksandr <rocketmannot@gmail.com>
2015-12-05 20:32:10 +0200

    [+] move fonts file to the production assets folder

--- config/assetPipelineManifest.js
+++ config/assetPipelineManifest.js
@@ -1,10 +1,24 @@
 var fs = require("fs"),
     path = require("path"),
+    ncp = require("ncp").ncp,
     mincer = require("mincer"),
     environment = require("./assetsPipelineEnvironment"),
     appConfig = require("./app_config");
 
+var movedFiles = ["bundles/fonts"];
+
+var moveFiles = function (files) {
+    files.forEach(function (file) {
+        ncp(path.join(appConfig.assetsPath, file), path.join(appConfig.compiledAssetsPath, file), function(err) {
+            if(err) {
+                console.log(err);
+            }
+            console.log("successfully moved files count:" + files.length);
+        });
+    });
+}
 var manifest = new mincer.Manifest(environment, appConfig.compiledAssetsPath);
+
 
 try{
     var compiled = manifest.compile([/(js)$|(css)$/], {
@@ -12,6 +26,9 @@ try{
         sourceMaps: true,
         embedMappingComments: true
     });
+
+    moveFiles(movedFiles);
+
 } catch (e) {
     console.error(e.stack );
 }
