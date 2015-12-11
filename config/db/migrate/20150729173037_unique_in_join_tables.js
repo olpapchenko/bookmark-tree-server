272ade6bd24e799427ef4ccb99e768583af2ded2
Oleksandr <rocketmannot@gmail.com>
2015-12-11 22:28:31 +0200

    [+] indexes

--- config/db/migrate/20150729173037_unique_in_join_tables.js
+++ config/db/migrate/20150729173037_unique_in_join_tables.js
@@ -2,13 +2,13 @@
 exports.up = function(knex, Promise) {
     return Promise.all([
     knex.schema.table("rights", function(t){
-        t.unique(["bookmark_id", "user_id"]);
+        t.unique(["user_id", "bookmark_id"]);
     }),
     knex.schema.table("bookmarks_bookmarks", function(t){
         t.unique(["bookmark", "references"]);
     })]),
     knex.schema.table("branches_users", function(t){
-        t.unique(["user_id", "branch_id"]);
+        t.unique(["branch_id", "user_id"]);
     })
 };
 
