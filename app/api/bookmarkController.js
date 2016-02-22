891d89aa56361dadbfcf9db31ef711dbe5525e6f
Oleksandr <rocketmannot@gmail.com>
2016-02-22 21:55:30 +0200

    [~] fix bookmark

--- app/api/bookmarkController.js
+++ app/api/bookmarkController.js
@@ -42,7 +42,7 @@ module.exports.allByBranch = actionComposer({
 module.exports.get = actionComposer({
     beforeFilters: [mandatoryParamFilter(["id"])],
     action: function (req, resp) {
-        User.forge({id: req.session.userId}).bookmark(req.query.id).fetch({withRelated: ["markers", "comments", "links"]}).then(function (m) {
+        User.forge({id: req.session.userId}).bookmark(req.query.id).fetchOne({withRelated: ["markers", "comments", "links"]}).then(function (m) {
             resp.json(m);
         });
     }
