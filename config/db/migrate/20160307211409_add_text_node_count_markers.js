6a03c96f7c5ebc68ff35bc92113fb5e7599700e7
Oleksandr <rocketmannot@gmail.com>
2016-03-08 13:57:08 +0200

    [+] order

--- config/db/migrate/20160307211409_add_text_node_count_markers.js
+++ config/db/migrate/20160307211409_add_text_node_count_markers.js
@@ -1,12 +1,14 @@
 
 exports.up = function(knex, Promise) {
     return knex.schema.table("markers", function (t) {
-        t.integer("textNodePosition");
+        t.integer("startTextNodePosition");
+        t.integer("endTextNodePosition");
     });
 };
 
 exports.down = function(knex, Promise) {
   return knex.schema.table("markers", function (t) {
-      t.dropColumn("textNodePosition");
+      t.dropColumn("startTextNodePosition");
+      t.dropColumn("endTextNodePosition");
   })
 };
