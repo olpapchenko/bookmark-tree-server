d7a48bc4caf3fb999ad4dcaab6db79f4336292e8
Oleksandr <rocketmannot@gmail.com>
2016-03-15 20:46:11 +0200

    [+] backstretch

--- assets/bundles/js/loginPage.js
+++ assets/bundles/js/loginPage.js
@@ -1,4 +1,3 @@
 //= require /js/vendor/jquery/jquery.min.js
 //= require /js/vendor/bootstrap/dist/js/bootstrap.js
 //= require /js/vendor/jquery-backstretch/jquery.backstretch.js
-//= require /js/vendor/placeholder.js
