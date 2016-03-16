5b229235c108bfb69d2d367d8d56a17a98914330
Oleksandr <rocketmannot@gmail.com>
2016-03-16 12:31:20 +0200

    [+] abstract model

--- app/models/AbstractModel.js
+++ app/models/AbstractModel.js
@@ -1,3 +1,18 @@
-/**
- * Created by user on 16.03.2016.
- */
+var Bookshelf = require ('../../config/db/bookshelf');
+var _ = require('underscore');
+
+var abstractModel = Bookshelf.Model.extend({
+    saveBasedOnParams : function (attrs, options) {
+        options = options || {};
+        return this.fetch().then(function (model) {
+            var isSaved = model == null;
+            model = model || this;
+            return model.save(attrs, _.extend({method: isSaved? 'insert' : 'update'}, options)).then(function () {
+                return isSaved;
+            });
+        })
+    }
+});
+
+Bookshelf.model("AbstractModel", abstractModel)
+module.exports = abstractModel;
