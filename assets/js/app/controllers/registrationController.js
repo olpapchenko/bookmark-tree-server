21f348264b39b14b08b0ce009ec768caa317cbfc
Oleksandr <rocketmannot@gmail.com>
2016-08-25 19:40:00 +0300

    [~] make better registration

--- assets/js/app/controllers/registrationController.js
+++ assets/js/app/controllers/registrationController.js
@@ -1,13 +1,21 @@
 define(["angular", "app", "services/userService"], function() {
-    angular.module("app").controller("registerController", ["userService", "$state", "$scope", function (userService, $state, $scope) {
+    angular.module("app").controller("registerController", ["userService", "$state", "$scope", "ngProgressFactory", "toaster",
+        function (userService, $state, $scope, ngProgressFactory, toaster) {
     $scope.register = function () {
         if($scope.register_form.$invalid) {
             return;
         }
 
         $scope.user.origin = 1;
+        var progress = ngProgressFactory.createInstance();
+        progress.start();
+
         userService.register($scope.user).then(function () {
             $state.go("app.overview");
+        }, function () {
+            toaster.pop('error', 'Error', 'Some error occurred');
+        }).finally(function () {
+            progress.complete();
         });
     }
 }]);});
