e55bcbbc540e59231d7f90ebec5fa9558ed85b7b
Oleksandr <rocketmannot@gmail.com>
2016-08-25 19:03:37 +0300

    [~] fix notifications

--- assets/js/app/controllers/notificationsController.js
+++ assets/js/app/controllers/notificationsController.js
@@ -1,4 +1,5 @@
-define(["angular", "app"], function() {angular.module("app").controller("notificationsController", ["$scope", "notifications", function ($scope, notifications) {
+define(["angular", "app", "moment", "moment-timezone-with-data-2010-2020"],
+    function() {angular.module("app").controller("notificationsController", ["$scope", "notifications", function ($scope, notifications) {
     $scope.notifications = notifications;
     $scope.notifications.forEach(function (item) {
         item.relative = moment.tz(item.created_at, 'America/Los_Angeles').fromNow();
