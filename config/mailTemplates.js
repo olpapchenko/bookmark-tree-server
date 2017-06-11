7a2e7e2e95d89a154ceb7c881e72b25316c6b2bc
Oleksandr <rocketmannot@gmail.com>
2017-06-11 12:15:55 +0300

    [~] fix mail template

--- config/mailTemplates.js
+++ config/mailTemplates.js
@@ -16,17 +16,15 @@ module.exports.mailVerificationTemplate = function (context) {
               <img style="margin-left: 10px; vertical-align: middle;" width="47" height="57" title="" alt="" src="${appConfig.baseUrl}images/mailIcon.png" />
               <h2 style="display: inline-block; font-family: sans-serif; font-weight: 100; color: white; vertical-align: middle;">BookmarkTree Chrome extension</h2>
             </header>
-            <div id="content" style=" height: 400px;">
-              <img id= "background" style="left: 0; opacity: 0.6; position: absolute; z-index: -1000;" src="${appConfig.baseUrl}/images/workplace.jpg">
+            <div id="content" style=" height: 400px;  background-image: url(${appConfig.baseUrl}images/workplace.jpg); text-align: center;">
               <h2 style="margin-top: 0px; padding-top: 30px; font-weight: 100; text-align: center; color: #1a3a1b; opacity: 1;">${context.name} Welcome to BookmarkTree extension!</h2>
               <h3 style="text-align: center; padding-top: 200px; font-weight: 100; text-align: center; color: #1a3a1b; opacity: 1;">
                  Bookmark tree will make you <span style="color: #e24242;">love</span> your bookmarks
               </h3>
+                  <a class="button" style = "text-decoration: none; font-family: sans-serif; font-weight: 200; padding: 10px; text-align: center; background-color: #17bb9c; border-radius: 10px; display: inline-block;
+                  color: white"href="${context.link}">Click to verify your mail
+                  </a>
             </div>
-            <footer style="text-align: center;">
-              <a class="button" style = "text-decoration: none; font-family: sans-serif; font-weight: 200; padding: 10px; text-align: center; background-color: #17bb9c; border-radius: 10px; display: inline-block;
-                  color: white"href="${context.link}">Click to verify your mail</a>
-            </footer>
         </div>
           </tr>
         </tbody>
